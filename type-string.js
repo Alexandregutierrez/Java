255f456246a3b7eb8b7e0e60f81915e5f31103c3
Alexandre Diaz Gutierrez <ale@MacBook-Air-de-Alexandre.local>
2023-02-25 17:59:53 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,27 @@
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo';
+const senha = "senhaSuperSegura123456!"
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const MeuNome = "Alexandre";
+
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+// concatenacao (+)
+console.log(citacao + MeuNome)
+
+
+
+// template 
